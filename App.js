7437537eb24510b2186daf2c62c879a29ed86710
Ramin <ramin.shrestha057@gmail.com>
2023-03-26 08:02:04 +0545

    practiced npm usage

---
+++ App.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const quotesData = require('./quotes.json');
+
+const PORT = 4000;
+//create a server
+const app = express();
+
+const getRandomIndex = () => {
+  return Math.floor(Math.random() * quotesData.quotes.length);
+};
+
+app.get('/', (req, res) => {
+  res.send('quotes app');
+});
+
+app.get('/quotes', (req, res) => {
+  res.json(quotesData.quotes[getRandomIndex()]);
+});
+app.listen(PORT, () => {
+  console.log(`server started at port ${PORT}`);
+});
+
+console.log('hello');
